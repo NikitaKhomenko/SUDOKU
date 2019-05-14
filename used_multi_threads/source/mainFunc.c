bb0468f9c0f12890fdf64c1c8303d839fb16fdb7
Nikita Khomenko <nickita1@gmail.com>
2019-05-14 12:10:33 +0300

    Update mainFunc.c

--- used_multi_threads/source/mainFunc.c
+++ used_multi_threads/source/mainFunc.c
@@ -13,13 +13,6 @@
 #include "../header/threadFunc.h"
 
 void fork_and_assign(pthread_t thread_rows[MAT_SIZE], pthread_t thread_cols[MAT_SIZE],
-                     pthread_t thread_subMat[MAT_SIZE], GAMESTATE *shared_gameState);
-void read_from_terminal_and_write_matrix(int matrix[MAT_SIZE][MAT_SIZE], GAMESTATE *shared_gameState);
-void read_from_file_and_write_matrix(int matrix[MAT_SIZE][MAT_SIZE], char *filename, GAMESTATE *shared_gameState);
-void check_sudoku(char* filePath, GAMESTATE *shared_gameState);
-void print_matrix(int matrix[MAT_SIZE][MAT_SIZE]);
-
-void fork_and_assign(pthread_t thread_rows[MAT_SIZE], pthread_t thread_cols[MAT_SIZE],
                      pthread_t thread_subMat[MAT_SIZE], GAMESTATE *shared_gameState) {
     int i;
     for (i = 0; i < MAT_SIZE; i++) {
